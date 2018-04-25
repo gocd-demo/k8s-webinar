c681f98a8a711053daff2813d090f5cc23fa9052
Akshay Dewan <akshayde@thoughtworks.com>
2018-04-25 13:49:27 +0530

    Add functional tests

---
+++ functional_tests/specs/example.spec
@@ -0,0 +1,7 @@
+# Guestbook test
+
+## Verify page
+* Go to homepage at "http://localhost:8081"
+* Check title is "Guestbook"
+* Enter "test" in textbox
+* Verify Submit
---
+++ functional_tests/tests/step_implementation.js
@@ -0,0 +1,24 @@
+/* globals gauge*/
+"use strict";
+const { openBrowser, closeBrowser, goto, title, $, write, into, button } = require('taiko');
+const assert = require("assert");
+
+beforeSuite(async () => openBrowser());
+
+afterSuite(async () => closeBrowser());
+
+
+step("Go to homepage at <query>", async query => goto(query));
+
+step("Check title is <word>", async (word) => {
+    let t = await title();
+    assert.equal(t, word)
+});
+
+step("Enter <word> in textbox", async (word) => {
+    await write(word, into($('input')));
+});
+
+step("Verify Submit", async() => {
+    await button('Submit').exists();
+});
