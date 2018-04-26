1d0b055e7f6a1dc0b12df03ab9d219442599043c
Akshay Dewan <akshayde@thoughtworks.com>
2018-04-26 15:23:38 +0530

    Fix the tag of the image to fetch

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-CONTAINER_ID=$(docker run -d -p8081:80 -ti gocddemo/guestbook:v13)
+CONTAINER_ID=$(docker run -d -p8081:80 -ti gocddemo/guestbook:v${GO_DEPENDENCY_LABEL_UPSTREAM})
 npm install --prefix functional_tests
 gauge install -d functional_tests
 gauge run specs -d functional_tests
