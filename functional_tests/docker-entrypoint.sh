3e58a2ae64580ec9af2069e23f412552b300b567
Sheroy Marker <smarker@thoughtworks.com>
2019-04-25 15:52:18 -0700

    Switching the test base image to alpine, upgrading Taiko

--- functional_tests/docker-entrypoint.sh
+++ functional_tests/docker-entrypoint.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+set -euo pipefail
+
+export TAIKO_BROWSER_PATH=/usr/bin/chromium-browser
+export TAIKO_SKIP_CHROMIUM_DOWNLOAD=true
 
 git clone https://github.com/gocd-demo/k8s-webinar.git
+
 npm install --prefix k8s-webinar/functional_tests; gauge install -d k8s-webinar/functional_tests; gauge run specs -d k8s-webinar/functional_tests
